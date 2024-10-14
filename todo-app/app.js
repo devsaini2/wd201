7ea35f214ae916bae95ab952b177eca78b09e366
root <root@LAPTOP-F84R5DI0>
2024-10-14 18:45:02 +0530

    Database model index file added  l6

--- todo-app/app.js
+++ todo-app/app.js
@@ -35,6 +35,38 @@ app.get("/todos", async (req, res) => {
     res.status(404).json({ error: "Failed to Fetch todos" });
   }
 });
+// video content
+app.get("/todos", (request, response) =>{
+  console.log("Todo List")
+})
+app.post("/todos", async (request, response) => {
+  console.log("creating a todo", request.body);
+  try{
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+    })
+    return response.json(todo);
+  } catch (error){
+    console.log(error);
+    return response.status(422).json(error);
+  }
+})
+app.put("/todos/:id/markAsCompleted", async (request, response) => {
+  console.log("we have to update a todo with id:", request.params.id);
+  const todo = await Todo.findByPk(request.params.id);
+  try{
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error){
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+/*app.delete("todos/:id", (request, response) => {
+  console.log("Deleted a todo by id:", request.params.id);
+});*/
 
 app.post("/todos", async (request, response) => {
   const { title, dueDate } = request.body;
@@ -94,35 +126,5 @@ app.delete("/todos/:id", async function (request, response) {
 });
 
 // video content
-app.get("/todos", (request, response) =>{
-  console.log("Todo List")
-})
-app.post("/todos", async (request, response) => {
-  console.log("creating a todo", request.body);
-  try{
-    const todo = await Todo.addTodo({
-      title: request.body.title,
-      dueDate: request.body.dueDate,
-    })
-    return response.json(todo);
-  } catch (error){
-    console.log(error);
-    return response.status(422).json(error);
-  }
-})
-app.put("/todos/:id/markAsCompleted", async (request, response) => {
-  console.log("we have to update a todo with id:", request.params.id);
-  const todo = await Todo.findByPk(request.params.id);
-  try{
-    const updatedTodo = await todo.markAsCompleted();
-    return response.json(updatedTodo);
-  } catch (error){
-    console.log(error);
-    return response.status(422).json(error);
-  }
-});
 
-/*app.delete("todos/:id", (request, response) => {
-  console.log("Deleted a todo by id:", request.params.id);
-});*/
 module.exports = app;
